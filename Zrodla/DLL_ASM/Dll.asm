432d5631f941d61ddb8a4f61d2c727ae29562abc
pakaras <paulina4510@gmail.com>
2016-11-20 22:51:29 +0100

    commit1

--- Zrodla/DLL_ASM/Dll.asm
+++ Zrodla/DLL_ASM/Dll.asm
@@ -2,8 +2,12 @@
 
 subtractionAsm PROC
 
-	mov [3DED4E750], 12
-	add ax, 12
+	EMMS			;clears registers for MMX use
+	MOVQ mm0, a[0]	;mm0 holds eight bytes of a
+	MOVQ mm1, b[0]	;mm1 holds eight bytes of b
+	PSUBB mm0, mm1	;mm0 holds eight bytes a+b
+	MOVQ c[0], mm0	;eight bytes of result put into memory
+	EMMS			;clears registers
 
 	ret
 
