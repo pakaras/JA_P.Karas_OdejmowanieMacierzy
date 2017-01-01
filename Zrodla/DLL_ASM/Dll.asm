310ac81103045b616cd488b77c63360b1b41c901
pakaras <paulina4510@gmail.com>
2017-01-01 23:03:19 +0100

    commit

--- Zrodla/DLL_ASM/Dll.asm
+++ Zrodla/DLL_ASM/Dll.asm
@@ -1,13 +1,17 @@
+.data
+tabA dw 100 dup (?)
+tabB dw 100 dup (?)
+
 .code
 
 subtractionAsm PROC
 
-	EMMS			;clears registers for MMX use
-	MOVQ mm0, a[0]	;mm0 holds eight bytes of a
-	MOVQ mm1, b[0]	;mm1 holds eight bytes of b
-	PSUBB mm0, mm1	;mm0 holds eight bytes a+b
-	MOVQ c[0], mm0	;eight bytes of result put into memory
-	EMMS			;clears registers
+	EMMS				;clears registers for MMX use
+	MOVQ mm0, tabA[0]	;mm0 holds eight bytes of a
+	MOVQ mm1, tabB[0]	;mm1 holds eight bytes of b
+	PSUBB mm0, mm1		;mm0 holds eight bytes a+b
+	MOVQ tabA[0], mm0	;eight bytes of result put into memory
+	EMMS				;clears registers
 
 	ret
 
